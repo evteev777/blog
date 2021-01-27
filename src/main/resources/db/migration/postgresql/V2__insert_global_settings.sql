cbb6c15a7d1253a81c82b3ed0e7e805b537105c3
Александр Евтеев <evteev777@gmail.com>
2021-01-27 08:43:46 +0300

    Added insert global settings to migration

---
+++ src/main/resources/db/migration/postgresql/V2__insert_global_settings.sql
@@ -0,0 +1,6 @@
+INSERT INTO global_settings(code, name, value)
+VALUES ('MULTIUSER_MODE','Многопользовательский режим', 'true');
+INSERT INTO global_settings(code, name, value)
+VALUES ('POST_PREMODERATION','Премодерация постов', 'true');
+INSERT INTO global_settings(code, name, value)
+VALUES ('STATISTICS_IS_PUBLIC','Показывать статистику', 'true');
